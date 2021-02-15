9c60a6d2616ff3e7a383e1edb690978f61d8ce1a
jopms <joao.34@hotmail.com>
2021-02-15 12:31:28 +0000

    Changed max height in javascript for note div and text div

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const storage = {
     nextIndex : 0
 }
 
-function Note (title = "Insert title here", text ="Insert text here", index){
+function Note (title = "Insert title here", text ="Insert text here", index, resize = 0){
     this.title = title;
     this.text = text;
     this.index = index;
+    this.resize = resize;
 }
 
 function toggleSideBar(){
@@ -54,6 +55,7 @@ function refreshInput (e){
 
 function closeNote(){
     removeNoteFromLocalStorage(this.parentElement.id);
+    
 }
 
 
@@ -70,10 +72,11 @@ function getDate(){
 function printNotesHtml(title, text, id){
     $(".notes-wrap").append(
     `
-    <div class ="note" id="${id}">
+    <div class ="note" id="note${id}">
         <div class="close"><span><i class="far fa-times-circle"></i></span></div>
         <div class="title-note"><span class="title" role="textbox" contenteditable>${title}</span></div>
-        <div class="text-note"><span class="text" role="textbox" contenteditable>${text}</span></div>
+        <div class="text-note"><span class="text" id="text${id}" role="textbox" contenteditable>${text}</span></div>
+        <div class ="resize"></div>
     </div> 
     `);
 }        
@@ -97,12 +100,20 @@ function addNote(){
     storage.nextIndex ++;
     localStorage.setItem('nextIndex', storage.nextIndex);
 }  
+
+function updateResize(){
+    console.log(this.parentElement.id);
+    updateLocalStorageData(this.parentElement.id, "resize");
+    
+    
+}
     
 function eventListener (){
     $(".close").each(function(){this.addEventListener("click", closeNote);})
     $(".title").each(function(){this.addEventListener("click", clearInput);})
     $(".text").each(function(){this.addEventListener("click", clearInput);})
     $("body")[0].addEventListener("click",refreshInput);
+    $(".resize").each(function(){this.addEventListener("click", updateResize);})
     $(".title").keyup(function (e) {
     let change = (e.target.innerHTML).replace(/&nbsp;/g, '');
     updateLocalStorageData (e.target.parentElement.parentElement.id,"title",change);
@@ -110,18 +121,37 @@ function eventListener (){
     $(".text").keyup(function (e) {  
     let change = (e.target.innerHTML).replace(/&nbsp;/g, '');
     updateLocalStorageData (e.target.parentElement.parentElement.id,"text",change);
+
+
     });
 }
 
 function updateLocalStorageData (id,whatChanged,variable){
     let temporaryList = JSON.parse(localStorage.getItem('list'));
     temporaryList.forEach(note => {
-        if(note.index == id){
+        if(note.index == id.match(/\d+/)[0]){
+            
             if(whatChanged === "title"){
                 note.title = variable;
             }
             if(whatChanged === "text"){
                 note.text = variable;
+            }
+            if(whatChanged === "resize"){
+                
+                note.resize=== 0 ? note.resize =1 : note.resize =0
+                if(note.resize ===1){
+                    $(`#${id}`).css({'max-height':'100%'});
+                    $('.resize').hover(function() {
+                        $(this).css("cursor","zoom-out")
+                      });
+                    $(`#text${id.match(/\d+/)}`).css({'max-height':'100%'});
+                }else{
+                    $(`#${id}`).css({'max-height':'20rem'});
+                    $('.resize').hover(function() {
+                        $(this).css("cursor","zoom-in")
+                      });
+                    $(`#text${id.match(/\d+/)}`).css({'max-height':'15rem'});}
             }
     }});
     localStorage.setItem('list', (JSON.stringify(temporaryList)) );
